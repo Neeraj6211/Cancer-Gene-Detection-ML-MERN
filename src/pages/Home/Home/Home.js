ba77f52ed8c9fe2bd8713ebe6d8774f7243adea5
Neeraj6211 <103454212+Neeraj6211@users.noreply.github.com>
2023-05-29 23:37:39 +0530

    Final HOme and Model

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -19,7 +19,7 @@ const Home = () => {
             <HomeServices/>
             <HomeOurTeam/>
             <HomeFact/>
-            <HomePricing/>
+            {/* <HomePricing/> */}
             <HomeCta/>
             <HomeBlogs/>
             <HomeFooter/>
